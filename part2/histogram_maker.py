2ff25da094e237607002574ab75e649c297028c3
SepehrNoey <sepehr.nk.81@aut.ac.ir>
2022-06-30 00:59:19 +0430

    refactor(part2): histogram_maker.py     renamed class HistogramMaker to HistogramHandler and refactored its methods for better use and less dependency, also added rgb2gray function

--- part2/histogram_maker.py
+++ part2/histogram_maker.py
@@ -1,59 +1,103 @@
 import numpy as np
 from PIL import Image
-from matplotlib import pyplot as plt
 
-class HistogramMaker:
-    def __init__(self, img_path, colorful: bool):
-        self.img_path = img_path
-        self.img = Image.open(img_path)
-        self.img_arr = np.array(self.img)
-        self.colorful = colorful
+
+class HistogramHandler:
+    def __init__(self, img_path=None, img_arr=None, channel_arr=None, is_color=False, make_cdf=True):
+        if img_path is not None:
+            self.__img_path = img_path
+            self.__img = Image.open(img_path)
+            self.__img_arr = np.array(self.__img)
+        elif img_arr is not None:
+            self.__img_arr = img_arr
+        elif channel_arr is not None:
+            self.__channel_arr = channel_arr
+
+        self.__color = is_color
         # now defining cdf histogram arrays
-        self.gray_cdf = np.zeros((1, 256))
-        self.red_cdf = np.zeros((1, 256))
-        self.green_cdf = np.zeros((1, 256))
-        self.blue_cdf = np.zeros((1, 256))
-        self.__make_cdf_histogram_array()
+        self.__gray_cdf = np.zeros((1, 256))
+        self.__red_cdf = np.zeros((1, 256))
+        self.__green_cdf = np.zeros((1, 256))
+        self.__blue_cdf = np.zeros((1, 256))
+        if make_cdf:
+            rgb = get_img_cdfs(img_arr)
+            if is_color:
+                self.__red_cdf = rgb[0]
+                self.__green_cdf = rgb[1]
+                self.__blue_cdf = rgb[2]
+            else:
+                self.__gray_cdf = rgb[0]
 
-    def __make_cdf_histogram_array(self):
-        if not self.colorful:
-            hist_arr = np.zeros((1, 256))
-            for i in range(self.img_arr.shape[0]):
-                for j in range(self.img_arr.shape[1]):
-                    hist_arr[0, self.img_arr[i, j]] += 1
+    def get__gray_cdf(self):
+        return self.__gray_cdf
 
-            self.gray_cdf[0] = hist_arr[0]
-            for i in range(1, 256):
-                self.gray_cdf[i] = self.gray_cdf[i - 1] + hist_arr[i]
+    def get__red_cdf(self):
+        return self.__red_cdf
 
-        else:
-            red_hist_arr = np.zeros((1, 256))
-            green_hist_arr = np.zeros((1, 256))
-            blue_hist_arr = np.zeros((1, 256))
-            for i in range(self.img_arr.shape[0]):
-                for j in range(self.img_arr.shape[1]):
-                    red_hist_arr[0, self.img_arr[i, j]] += 1
-                    green_hist_arr[0, self.img_arr[i, j]] += 1
-                    blue_hist_arr[0, self.img_arr[i, j]] += 1
+    def get__green_cdf(self):
+        return self.__green_cdf
 
-            self.red_cdf[0] = red_hist_arr[0]
-            self.green_cdf[0] = green_hist_arr[0]
-            self.blue_cdf[0] = blue_hist_arr[0]
-            for i in range(1, 256):
-                self.red_cdf[0, i] = self.red_cdf[0, i - 1] + red_hist_arr[0, i]
-                self.green_cdf[0, i] = self.green_cdf[0, i - 1] + green_hist_arr[0, i]
-                self.blue_cdf[0, i] = self.blue_cdf[0, i - 1] + blue_hist_arr[0, i]
+    def get__blue__cdf(self):
+        return self.__blue_cdf
 
+    def is_colorful(self):
+        return self.__color
 
+    def set__gray_cdf(self, gray_cdf):
+        self.__gray_cdf = gray_cdf
 
+    def set__red_cdf(self, red_cdf):
+        self.__red_cdf = red_cdf
 
+    def set__green_cdf(self, green_cdf):
+        self.__green_cdf = green_cdf
 
-
-
-    # def print(self):
-    #     plt.bar(range(256), self.red_cdf.reshape((256, )), color='red')
-    #     print("here")
+    def set__blue_cdf(self, blue_cdf):
+        self.__blue_cdf = blue_cdf
 
 
+def make_channel_hist_arr(channel_arr: np.ndarray):
+    hist_arr = np.zeros((1, 256))
+    for i in range(channel_arr.shape[0]):
+        for j in range(channel_arr.shape[1]):
+            hist_arr[0, channel_arr[i, j]] += 1
+    return hist_arr
 
 
+def make_cdf_hist(hist_arr: np.ndarray):
+    cdf = np.zeros((1, 256))
+    cdf[0, 0] = hist_arr[0, 0]
+    for j in range(1, 256):
+        cdf[0, j] = cdf[0, j - 1] + hist_arr[0, j]
+    return cdf
+
+
+def get_img_cdfs(img_arr):
+    cdf_list = []
+    for i in range(img_arr.shape[2]):
+        cdf_list.append(make_cdf_hist(make_channel_hist_arr(img_arr[:, :, i])))
+
+    return cdf_list
+
+
+# def match(src: HistogramHandler, ref: HistogramHandler):
+#     if ref.is_colorful() and (not src.is_colorful()):
+#         print("Can't match grayscale image to rgb image yet!")
+#     else:
+#         if ref.is_colorful() and src.is_colorful():
+#
+#         elif (not ref.is_colorful()) and src.is_colorful():
+#
+#         else:
+#
+# def map_channel(src_cdf, ref_cdf):
+#
+#     for i in range(256):
+
+
+def rgb2gray(color_img):
+    return color_img.convert('L')
+
+# def print(self):
+#     plt.bar(range(256), self.red_cdf.reshape((256, )), color='red')
+#     print("here")
